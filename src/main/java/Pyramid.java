b633844eaebfb4f3c609d28225f5a2b416ac07c6
vtran91 <vtran16@bellsouth.net>
2023-02-27 15:41:50 +0000

    Completed Pyramid Coding Challenge

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String str = "";
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+                str += "*";
+            str += "\n";
+        }
+        return str;
     }
 }
